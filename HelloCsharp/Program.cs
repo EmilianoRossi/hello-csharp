2a0a232948728d9e73f5701e509302dc29c8e703
EmilianoRossi <emiliano.rossi658@gmail.com>
2022-03-31 15:23:22 +0200

    Add project files.

---
+++ HelloCsharp/Program.cs
@@ -0,0 +1,15 @@
+﻿static void Main(string[] args)
+{   
+
+    Console.WriteLine("Hello, World!");
+    //Dichiaro una matrice di stringhe contenente i mesi dell'anno
+    string[] mesiDellAnno = { "Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre" };
+    //Scorro la matrice e stampo andando a capo tutti i mesi dell'anno
+    for(int i = 0; i < mesiDellAnno.Length; i++)
+    {
+
+        Console.WriteLine(mesiDellAnno[i]);
+
+    }
+
+}
