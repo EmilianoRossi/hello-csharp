f74d42f726be364e9bd118f7b579d22a62617b40
EmilianoRossi <emiliano.rossi658@gmail.com>
2022-03-31 15:32:15 +0200

    dichiaro matrice stringhe con mesi dell'anno e stampo i mesi a capo.

--- HelloCsharp/Program.cs
+++ HelloCsharp/Program.cs
@@ -1,15 +1,16 @@
-﻿static void Main(string[] args)
-{   
+﻿Console.WriteLine("Hello, World!");
 
-    Console.WriteLine("Hello, World!");
-    //Dichiaro una matrice di stringhe contenente i mesi dell'anno
-    string[] mesiDellAnno = { "Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre" };
-    //Scorro la matrice e stampo andando a capo tutti i mesi dell'anno
-    for(int i = 0; i < mesiDellAnno.Length; i++)
-    {
+//Dichiaro una matrice di stringhe contenente i mesi dell'anno
+string[] mesiDellAnno = { "Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre" };
 
-        Console.WriteLine(mesiDellAnno[i]);
+//Scorro la matrice e stampo andando a capo tutti i mesi dell'anno
+for (int i = 0; i < mesiDellAnno.Length; i++)
+{
 
-    }
+    Console.WriteLine(mesiDellAnno[i]);
 
 }
+
+           
+
+       
